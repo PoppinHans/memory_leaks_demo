6131b1886bd4ad83e1354db2d467305a1be0ae7e
hsji <hsji@thoughtworks.com>
2020-03-30 20:52:27 +0800

    feat: added code base for memory leak demo.

---
+++ main.cc
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main() {
+    //1. instance over dynamically allocated memory
+    //2. smart pointers to transfer ownership
+    //3. virtual destructor for base classes
+    return 0;
+}
